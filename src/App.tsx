3470c874aea6089534479a51f6dd5e4deba16c55
buzulanandrew525 <buzulanandrew525@gmail.com>
2025-04-08 22:53:41 +0300

    Added loading, changed styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FadeLoader } from "react-spinners";
 import "./index.css";
 
 const API_KEY = "7f8e81002b7e4ef29e8b195bae65802f";
@@ -12,19 +13,23 @@ interface WeatherData {
 function App() {
   const [city, setCity] = useState<string>("");
   const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchWeather = async () => {
     if (!city) return;
 
     try {
+      setIsLoading(true);
       const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=ru`
       );
       const data = await res.json();
       if (data.cod === 200) {
+        setIsLoading(false);
         setWeather(data);
       } else {
         alert("Город не найден");
+        setIsLoading(false);
         setWeather(null);
       }
     } catch (error) {
@@ -43,13 +48,17 @@ function App() {
       />
       <button onClick={fetchWeather}>Узнать погоду</button>
 
-      {weather && (
-        <div className="weather">
-          <h2>{weather.name}</h2>
-          <p>{weather.weather[0].description}</p>
-          <p>Температура: {weather.main.temp}°C</p>
-          <p>Влажность: {weather.main.humidity}%</p>
-        </div>
+      {isLoading ? (
+        <FadeLoader color="#000000" height={15} width={5} radius={2} margin={2} />
+      ) : (
+        weather && (
+          <div className="weather">
+            <h2>{weather.name}</h2>
+            <p>{weather.weather[0].description}</p>
+            <p>Температура: {weather.main.temp}°C</p>
+            <p>Влажность: {weather.main.humidity}%</p>
+          </div>
+        )
       )}
     </div>
   );
